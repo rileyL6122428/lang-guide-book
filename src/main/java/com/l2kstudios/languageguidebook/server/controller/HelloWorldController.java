70f0352a285d37d48fd60406409ad2b6f2eb0141
Riley Littlefield <rileylittlefield@ymail.com>
2017-07-07 00:16:38 -0400

    adds basic json response in hello world controller

--- src/main/java/com/l2kstudios/languageguidebook/server/controller/HelloWorldController.java
+++ src/main/java/com/l2kstudios/languageguidebook/server/controller/HelloWorldController.java
@@ -3,13 +3,15 @@ package com.l2kstudios.languageguidebook.server.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.l2kstudios.languageguidebook.server.model.HelloWorld;
+
 @RestController
 public class HelloWorldController {
 	
 	@GetMapping(path = "/helloworld", produces = "application/json")
-	public String index() {
+	public Object index() {
 		
-		return "Hello world";
+		return new HelloWorld();
 	}
 	
 }
---
+++ src/main/java/com/l2kstudios/languageguidebook/server/model/HelloWorld.java
@@ -0,0 +1,20 @@
+package com.l2kstudios.languageguidebook.server.model;
+
+public class HelloWorld {
+	
+	private String message;
+	
+	{
+		setMessage("Hello World");
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	
+}
