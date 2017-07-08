de1a05a66cfbb7947d2fd0a66ed6e2d66c2d3419
Riley Littlefield <rileylittlefield@ymail.com>
2017-07-08 18:11:20 -0400

    adds heroku deployment script

---
+++ deploy-build.sh
@@ -0,0 +1,22 @@
+./node_modules/karma/bin/karma start --single-run
+karma_test_return_code=$?
+
+if [ "$karma_test_return_code" -ne "0" ]
+then
+  echo "FRONTEND BUILD FAILURE"
+  exit 1
+fi
+
+
+mvn clean test -o
+maven_test_return_code=$?
+
+if ["$maven_test_return_code" != "0"]
+then
+  echo "BACKEND BUILD FAILURE"
+  exit 1
+fi
+
+
+branch_name=$1
+git push heroku $branch_name
