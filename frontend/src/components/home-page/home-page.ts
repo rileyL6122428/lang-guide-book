7cf2a026132e1a75326a131a67d19a87106b676b
Riley Littlefield <rileylittlefield@ymail.com>
2017-07-09 16:05:46 -0400

    adds basic styling to Home page

--- frontend/src/components/home-page/home-page.ts
+++ frontend/src/components/home-page/home-page.ts
@@ -2,7 +2,30 @@ import { Component } from '@angular/core';
 
 @Component({
   template: `
-    <h1>Language Guide Book</h1>
+  <section id="home-page">
+    <h1>
+      <div id="header-image">(Image Place Holder)</div>
+      <p id="header-text">Language Guide Book</p>
+    </h1>
+    <p id="welcome-paragraph">
+      Welcome to the Language Guide Book, a guide book for video games played
+      in foreign languages!
+    </p>
+
+    <section id="what-you-can-do">
+      <p>On this site, you can:</p>
+      <ul>
+        <li>Create and manage translations</li>
+        <li>Read through and download other people's translations</li>
+      </ul>
+    </section>
+
+    <section id="getting-started">
+      <p>New users can click here to get started.</p>
+      <p>Returning users can click here to login.</p>
+    </section>
+
+  </section>
   `,
 })
 export class HomePageComponent { }
