56db3d9202fc6a53ffafcb743f541e396f5f3481
Riley Littlefield <rileylittlefield@ymail.com>
2017-07-09 16:16:32 -0400

    updates build script to test front-end build process before deploy; fixes tsconfig.json file reference

--- deploy-build.sh
+++ deploy-build.sh
@@ -1,9 +1,19 @@
+webpack
+webpack_return_code=$?
+if ["$webpack_return_code" != "0"]
+then
+  echo "FRONTEND BUILD FAILURE"
+  exit 1
+fi
+
+
+
 ./node_modules/karma/bin/karma start --single-run
 karma_test_return_code=$?
 
 if [ "$karma_test_return_code" -ne "0" ]
 then
-  echo "FRONTEND BUILD FAILURE"
+  echo "FRONTEND TEST FAILURE"
   exit 1
 fi
 
@@ -13,10 +23,11 @@ maven_test_return_code=$?
 
 if ["$maven_test_return_code" != "0"]
 then
-  echo "BACKEND BUILD FAILURE"
+  echo "BACKEND BUILD OR TEST FAILURE"
   exit 1
 fi
 
 
+
 branch_name=$1
-git push heroku $branch_name
+git push heroku "$branch_name:master"
