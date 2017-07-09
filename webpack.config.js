209e57a33d52d5a6827d5d1fcf1e50582773a80e
Riley Littlefield <rileylittlefield@ymail.com>
2017-07-09 14:06:28 -0400

    sets up sass with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,41 @@
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports = {
-    entry: "./frontend/src/bootstrap.ts",
+    entry: ["./frontend/src/bootstrap.ts", "./frontend/styles/language-guide-book.scss"],
     output: {
         filename: "src/main/webapp/bundle.js"
     },
+
+    // rules: [
+    //   {
+    //     test: /\.css$/,
+    //     loader: ExtractTextPlugin.extract({
+    //       loader: 'css-loader?importLoaders=1',
+    //     })
+    //   },
+    //
+    //
+    // ],
+
     resolve: {
         extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
     },
     module: {
         loaders: [
             // all files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'
-            { test: /\.tsx?$/, loader: "ts-loader" }
+            { test: /\.tsx?$/, loader: "ts-loader" },
+
+            { // sass / scss loader for webpack
+              test: /\.(sass|scss)$/,
+              loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
+            }
         ]
-    }
+    },
+
+    plugins: [
+      new ExtractTextPlugin({ // define where to save the file
+        filename: 'src/main/webapp/bundle.css',
+        allChunks: true,
+      })
+    ]
 }
