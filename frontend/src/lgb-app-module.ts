e9265dd1bf5506b4e7455f2fed68dff0b76cddb2
Riley Littlefield <rileylittlefield@ymail.com>
2017-07-12 19:59:16 -0400

    fixes refresh issue with router

--- frontend/src/lgb-app-module.ts
+++ frontend/src/lgb-app-module.ts
@@ -3,20 +3,22 @@ import 'reflect-metadata';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { RouterModule } from '@angular/router'
 import { routes } from './routes'
 
 import { HomePageComponent } from './components/home-page/home-page'
 import { LGBAppComponent } from './lgb-app-component'
 
-
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes),
+    RouterModule.forRoot(routes, { useHash: true }),
     BrowserModule,
     FormsModule,
   ],
   declarations: [ HomePageComponent, LGBAppComponent ],
-  bootstrap:    [ LGBAppComponent ]
+  bootstrap:    [ LGBAppComponent ],
+  providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy}]
 })
 export class LGBAppModule { }
--- frontend/src/routes.ts
+++ frontend/src/routes.ts
@@ -3,5 +3,6 @@ import { HomePageComponent } from './components/home-page/home-page'
 
 export const routes: Routes = [
   { path: 'home', component: HomePageComponent },
-  { path: '', redirectTo: 'home', pathMatch: 'full' }
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', redirectTo: 'home' }
 ];
