e1b41357f15fc9fac8615da7882717338e0063fe
Riley Littlefield <rileylittlefield@ymail.com>
2017-08-20 14:07:12 -0400

    adds test data

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,9 @@
 INSERT INTO translator (name, encrypted_password) VALUES ('l2k', '$2a$10$hXDvrU7i7c0kkA2T53FQsuj7E5pq0GBaV3/tVk.9YHK2293sCM1Ty')
 
 INSERT INTO translation (id, name, translator_name) VALUES (1, 'Tales of Berseria', 'l2k')
+INSERT INTO translation (id, name, translator_name) VALUES (2, 'Nier Automata', 'l2k')
+INSERT INTO translation (id, name, translator_name) VALUES (3, 'Persona 5', 'l2k')
+INSERT INTO translation (id, name, translator_name) VALUES (4, 'Fire Emblem Echoes', 'l2k')
+INSERT INTO translation (id, name, translator_name) VALUES (5, 'Final Fantasy 15', 'l2k')
+
+INSERT INTO translator (name, encrypted_password) VALUES ('l2k-no-works', '$2a$10$hXDvrU7i7c0kkA2T53FQsuj7E5pq0GBaV3/tVk.9YHK2293sCM1Ty')
